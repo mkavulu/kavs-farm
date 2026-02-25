fc7c329577a6a736ab5c9c25fdb4aeeb1dbefa4a
Moses Kavulu <mutukukavulu2000@gmail.com>
2026-02-25 09:56:48 +0300

    made changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,43 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className="navbar">
-      <h2>Kavs Farm Enterprises</h2>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/products">Products</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+      <h1>Kavs Farm Enterprises</h1>
+
+      {/* Hamburger Menu */}
+      <span className="menu-toggle" onClick={toggleMenu}>
+        ☰
+      </span>
+
+      <ul className={isOpen ? "active" : ""}>
+        <li>
+          <Link to="/" onClick={closeMenu}>Home</Link>
+        </li>
+        <li>
+          <Link to="/products" onClick={closeMenu}>Products</Link>
+        </li>
+        <li>
+          <Link to="/about" onClick={closeMenu}>About</Link>
+        </li>
+        <li>
+          <Link to="/contact" onClick={closeMenu}>Contact</Link>
+        </li>
       </ul>
     </nav>
   );
 }
+
 export default Navbar;
