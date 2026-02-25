ddb26192b85e29a77fe152e1decc53e43532e03f
Moses Kavulu <mutukukavulu2000@gmail.com>
2026-02-25 14:13:54 +0300

    made changes

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import "./Gallery.css";
+
 import farm1 from "../assets/farm1.jpg";
 import farm2 from "../assets/farm2.jpg";
 import farm3 from "../assets/farm3.jpg";
@@ -6,8 +8,21 @@ import veggiesImg from "../assets/veggies.jpg";
 import milkImg from "../assets/milk.jpg";
 import eggsImg from "../assets/eggs.jpg";
 
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+
 function Gallery() {
-  const photos = [farm1, farm2, farm3, veggiesImg, milkImg, eggsImg];
+  const photos = [
+    { src: farm1, title: "Farm Fields" },
+    { src: farm2, title: "Fresh Produce" },
+    { src: farm3, title: "Livestock" },
+    { src: veggiesImg, title: "Organic Vegetables" },
+    { src: milkImg, title: "Fresh Milk" },
+    { src: eggsImg, title: "Free-range Eggs" }
+  ];
+
+  const [open, setOpen] = useState(false);
+  const [index, setIndex] = useState(0);
 
   return (
     <section className="gallery">
@@ -17,14 +32,31 @@ function Gallery() {
       </div>
 
       <div className="gallery-grid">
-        {photos.map((photo, index) => (
-          <div className="gallery-card" key={index}>
-            <img src={photo} alt={`Gallery ${index + 1}`} />
+        {photos.map((photo, i) => (
+          <div
+            className="gallery-card"
+            key={i}
+            onClick={() => {
+              setIndex(i);
+              setOpen(true);
+            }}
+          >
+            <img src={photo.src} alt={photo.title} />
           </div>
         ))}
       </div>
+
+      {open && (
+        <Lightbox
+          open={open}
+          close={() => setOpen(false)}
+          index={index}
+          slides={photos}
+        />
+      )}
     </section>
   );
 }
 
 export default Gallery;
+
